49f7ba2326bd00b520e22cf1e010b826deb0900d
Đào Trọng Linh <24022678@vnu.edu.vn>
2025-09-24 14:59:48 +0700

    add

---
+++ src/Week1/Linked_List/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include "DefLinkedList.h"
+
+using namespace std;
+
+int main()
+{
+//TEST
+/*
+    LinkedList a;
+
+    a.DuyetXuoi();
+    a.DuyetNguoc();
+    a.XoaDau();
+    a.XoaCuoi();
+
+    a.ThemVaoDau(5);
+    a.ThemVaoDau(4);
+    a.ThemVaoDau(3);
+    a.ThemVaoDau(2);
+    a.ThemVaoDau(1);
+    a.DuyetXuoi();
+    a.DuyetNguoc();
+    a.TruyCap(6);
+
+    a.ThemVaoCuoi(1);
+    a.ThemVaoCuoi(2);
+    a.ThemVaoCuoi(3);
+    a.ThemVaoCuoi(4);
+    a.ThemVaoCuoi(5);
+    a.DuyetXuoi();
+    a.DuyetNguoc();
+    a.TruyCap(6);
+
+    a.ThemVaoViTri(6, 20);
+    a.DuyetXuoi();
+    a.DuyetNguoc();
+    a.TruyCap(6);
+
+    a.ThemVaoViTri(100, 30);
+    a.DuyetXuoi();
+    a.DuyetNguoc();
+    a.TruyCap(6);
+
+    a.XoaDau();
+    a.DuyetXuoi();
+    a.DuyetNguoc();
+    a.TruyCap(6);
+
+    a.XoaCuoi();
+    a.DuyetXuoi();
+    a.DuyetNguoc();
+    a.TruyCap(6);
+
+    a.XoaViTri(6);
+    a.DuyetXuoi();
+    a.DuyetNguoc();
+    a.TruyCap(6);
+
+    a.XoaViTri(-1);
+    a.DuyetXuoi();
+    a.DuyetNguoc();
+    a.TruyCap(6);
+*/
+    return 0;
+}
